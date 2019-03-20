00183cbf62e03387e140f69e1c4fa54474ef0654
erv <ervinmitra@gmail.com>
2019-03-20 08:06:47 -0700

    making team viewer

--- src/components/TeamBuilderApp.js
+++ src/components/TeamBuilderApp.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import TeamViewer from './TeamViewer';
+
+// make a calll to axios
+async function pullAPI(){
+  let pokeArray = [];
+  for(let i = 1; i < 6; ++i){
+    pokeArray[i] = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    pokeArray[i] = pokeArray[i].data;
+  }
+  return pokeArray;
+}
 
 export default class TeamBuilderApp extends React.Component {
+
   render() {
-    return <div>Build a team!</div>;
+    const pokes = pullAPI();
+    //console.log(team);
+    return <div><TeamViewer team={pokes} onRemoveFromTeam="" /></div>;
   }
 }
--- src/components/TeamViewer.js
+++ src/components/TeamViewer.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
+import TeamViewItem from './TeamViewItem';
 
 export default class TeamViewer extends Component{
 
     render(){
         // destructure object
         const {team, onRemoveFromTeam} = this.props;
+        console.log(team);
         return(
-            <div className="team-viewer">
+            <div>
                 {team.map(pokemon => (
                     <div>
-                        <TeamViewItem key={pokemon.id} pokemon={pokemon}
+                        <TeamViewItem key={pokemon.id} pokemon={pokemon.name}
                         onDeleteMe={() => onRemoveFromTeam(pokemon.id)} />
                     </div>
                 ))}
